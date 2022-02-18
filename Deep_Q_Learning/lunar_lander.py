07dff3abef8dfd87dba9bb192fc6f5313bee43fb
Jake Mehlman <89606961+jdm365@users.noreply.github.com>
2022-02-18 07:10:59 -0500

    Finished running actor_critic_lunar_lander]

--- Deep_Q_Learning/lunar_lander.py
+++ Deep_Q_Learning/lunar_lander.py
@@ -1,50 +1,70 @@
+from turtle import forward
 import torch as T
 import numpy as np
 import torch.nn as nn
+import torch.optim as optim
 
-class Agent():
-    def __init__(self, lr, gamma, n_actions, state_space, eps_start, eps_end, 
-        eps_dec):
-        self.lr = lr
+class ActorCriticNetwork(nn.Module):
+    def __init__(self, lr, n_actions, input_dims, fc1_dims, fc2_dims):
+        super(ActorCriticNetwork, self).__init__()
+        self.actor_network = nn.Sequential(
+            nn.Linear(*input_dims, fc1_dims),
+            nn.ReLU(),
+            nn.Linear(fc1_dims, fc2_dims),
+            nn.ReLU(),
+            nn.Linear(fc2_dims, n_actions),
+            nn.Softmax()
+        )
+
+        self.critic_network = nn.Sequential(
+            nn.Linear(*input_dims, fc1_dims),
+            nn.ReLU(),
+            nn.Linear(fc1_dims, fc2_dims),
+            nn.ReLU(),
+            nn.Linear(fc2_dims, 1)
+        )
+
+        self.optimizer = optim.Adam(self.parameters(), lr=lr)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        self.to(self.device)
+
+    def forward(self, state):
+        pi = self.actor_network(state)
+        v = self.critic_network(state)
+        return pi, v
+
+class Agent:
+    def __init__(self, lr, input_dims, fc1_dims, fc2_dims, n_actions, \
+        gamma=0.99):
         self.gamma = gamma
-        self.epsilon = eps_start
-        self.eps_end = eps_end
-        self.eps_dec = eps_dec
-        self.n_actions = n_actions
+        self.actor_critic = ActorCriticNetwork(lr, n_actions, input_dims, \
+            fc1_dims, fc2_dims)
+        self.log_prob = None
 
-        self.state_space = state_space
-        self.actions = [i for i in range(self.n_actions)]
+    def choose_action(self, observation):
+        state = T.tensor([observation], dtype=T.float).to(self.actor_critic.device)
+        probs, _ = self.actor_critic.forward(state)
+        action_probs = T.distributions.Categorical(probs)
+        action = action_probs.sample()
+        self.log_prob = action_probs.log_prob(action)
+        return action.item()
 
-        self.Q = {}
+    def learn(self, state, reward, state_, done):
+        self.actor_critic.optimizer.zero_grad()
 
-        self.init_Q()
+        state = T.tensor([state], dtype=T.float).to(self.actor_critic.device)
+        reward = T.tensor([reward], dtype=T.float).to(self.actor_critic.device)
+        state_ = T.tensor([state_], dtype=T.float).to(self.actor_critic.device)
 
-    def init_Q(self):
-        for state in self.state_space:
-            for action in self.actions:
-                self.Q[(state, action)] = 0.0
-    
-    def max_action(self, state):
-        actions = np.array([self.Q[(state, a)] for a in self.actions])
-        action = np.argmax(actions)
-        return action
+        _, critic_value = self.actor_critic.forward(state)
+        _, critic_value_ = self.actor_critic.forward(state_)
 
-    def choose_action(self, state):
-        if np.random.random() < self.epsilon:
-            action = np.random.choice(self.action_space)
-        else:
-            action = self.max_action(state)
-        return action
+        delta = reward + self.gamma * critic_value_ * (1 - int(done)) - critic_value
+        actor_loss = -self.log_prob * delta
+        critic_loss = delta ** 2
 
-    def decrement_epsilon(self):
-        self.epsilon = self.epsilon - self.eps_dec \
-            if self.epsilon > self.eps_end \
-            else self.eps_end
-    
-    def learn(self, state, action, reward, state_):
-        a_max = self.max_action(state_)
-        self.Q[(state, action)] = self.Q[(state, action)] + \
-            self.lr * (reward + self.gamma * self.Q[(state_, a_max)] - \
-            self.Q[(state, action)])
+        (actor_loss + critic_loss).backward()
+        self.actor_critic.optimizer.step()
+
 
     
--- Deep_Q_Learning/lunar_lander_main.py
+++ Deep_Q_Learning/lunar_lander_main.py
@@ -4,3 +4,29 @@ from utils import plot_learning
 from lunar_lander import Agent
 
 env = gym.make('LunarLander-v2')
+num_episodes = 3000
+agent = Agent(lr=5e-6, input_dims=[8], n_actions=4, fc1_dims=2058, fc2_dims=1536, gamma=0.99)
+
+filename = 'Actor_critic_lunar_lander.png'
+
+scores = []
+for i in range(num_episodes):
+    done = False
+    observation = env.reset()
+    score = 0
+    while not done:
+        action = agent.choose_action(observation)
+        observation_, reward, done, _ = env.step(action)
+        score += reward
+        agent.learn(observation, reward, observation_, done)
+        observation = observation_
+
+        if i % 100 > 5 and i % 100 < 10:
+            env.render()
+    scores.append(score)
+
+    avg_score = np.mean(scores[-100:])
+    print('Episode ', i, 'score %.1f' % score, \
+        'average score %.1f' % avg_score)
+x = [i+1 for i in range(num_episodes)]
+plot_learning(scores, filename=filename, x=x)
