4ba32fc7a32cbebf71990c1199dc15c51c9d4851
jdm365 <jdm365@georgetown.edu>
2022-02-22 15:13:56 -0500

    Added vpg aka reinforce algorithm

---
+++ Vanilla_PG/main.py
@@ -0,0 +1,27 @@
+import gym
+import numpy as np
+from policy_gradient import Agent
+from utils import plot_learning
+
+env = gym.make('LunarLander-v2')
+agent = Agent(lr=1e-4, input_dims=env.observation_space.shape, \
+    n_actions=env.action_space.n)
+n_episodes = 5000
+score_history = []
+
+for i in range(n_episodes):
+    observation = env.reset()
+    done = False
+    score = 0
+    while not done:
+        action = agent.choose_action(observation)
+        observation_, reward, done, _ = env.step(action)
+        agent.remember(reward)
+        score += reward
+        observation = observation_
+        env.render()
+    score_history.append(score)
+    agent.learn()
+    print(f'Episode {i+1} score: {np.round(score, 2)} \t\t Average score: {np.round(np.mean(score_history[-100:]), 2)}')
+x = [x for x in range(n_episodes)]
+plot_learning(x, score_history, 'REINFORCE_lunar_lander.png')
---
+++ Vanilla_PG/policy_gradient.py
@@ -0,0 +1,68 @@
+import torch as T
+import torch.nn as nn
+import torch.optim as optim
+from torch.distributions import Categorical
+import numpy as np
+
+class ActorNetwork(nn.Module):
+    def __init__(self, lr, input_dims, fc1_dims, fc2_dims, n_actions):
+        super(ActorNetwork, self).__init__()
+
+        self.actor = nn.Sequential(
+            nn.Linear(*input_dims, fc1_dims),
+            nn.ReLU(),
+            nn.Linear(fc1_dims, fc2_dims),
+            nn.ReLU(),
+            nn.Linear(fc2_dims, n_actions),
+            nn.Softmax(dim=0)
+        )
+
+        self.optimizer = optim.Adam(self.parameters(), lr=lr)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        self.to(self.device)
+
+    def forward(self, observation):
+        observation = T.tensor(observation, dtype=T.float).to(self.device)
+        mu = Categorical(self.actor(observation))
+        return mu
+
+class Agent:
+    def __init__(self, lr, input_dims,  n_actions, fc1_dims=256, fc2_dims=256, gamma=0.99):
+        self.actor = ActorNetwork(lr, input_dims, fc1_dims, fc2_dims, n_actions)
+        self.log_probs_memory = []
+        self.reward_memory = []
+        self.gamma = gamma
+
+    def choose_action(self, observation):
+        mu = self.actor.forward(observation)
+        action = mu.sample()
+        log_probs = mu.log_prob(action)
+        self.log_probs_memory.append(log_probs)
+        return action.item()
+
+    def remember(self, reward):
+        self.reward_memory.append(reward)
+    
+    def learn(self):
+        discounted_rewards = []
+        for t in range(len(self.reward_memory)):
+            disc_rewards = 0
+            gamma = 1
+            for reward in self.reward_memory[t:]:
+                disc_rewards += reward * gamma
+                gamma *= self.gamma
+            discounted_rewards.append(disc_rewards)
+        
+        discounted_rewards = T.tensor(discounted_rewards, dtype=T.float).to(self.actor.device)
+        log_probs = T.tensor(self.log_probs_memory, dtype=T.float).to(self.actor.device)
+
+        policy_loss = 0
+        for return_estimate, log_probs in zip(discounted_rewards, self.log_probs_memory):
+            policy_loss -= return_estimate * log_probs
+        
+        self.actor.optimizer.zero_grad()
+        policy_loss.backward()
+        self.actor.optimizer.step()
+
+        self.log_probs_memory = []
+        self.reward_memory = []
