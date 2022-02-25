806fb5f878f35db6b0ca05407fb74dc2e47af00c
jdm365 <jdm365@georgetown.edu>
2022-02-24 19:44:34 -0500

    ___

--- PPO/proximal_policy_optimization.py
+++ PPO/proximal_policy_optimization.py
@@ -4,28 +4,45 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.distributions import Categorical
 from utils import init_linear
+import sys
 
 
 class ReplayBuffer:
     def __init__(self, batch_size):
         self.batch_size = batch_size
+        self.gamma = 0.99
+        self.gae_lambda = 0.95
 
         self.states = []
         self.actions = []
         self.rewards = []
-        self.states_ = []
         self.vals = []
         self.probs = []
         self.dones = []
 
-    def remember(self, state, action, reward, state_, val, probs, done):
+    def remember(self, state, action, reward, val, probs, done):
         self.states.append(state)
         self.actions.append(action)
         self.rewards.append(reward)
-        self.states_.append(state_)
         self.vals.append(val)
         self.probs.append(probs)
         self.dones.append(done)
+
+    def calc_advantages(self):
+        advantages = []
+        for t in range(len(self.rewards)-1):
+            discount = 1
+            A = 0
+            for k in range(len(self.rewards)-1):
+                terminal = 1 - int(self.dones[k])
+                delta = self.rewards[k] + self.gamma * self.vals[k+1] * terminal - self.vals[k]
+                A += discount * delta
+                discount *= self.gamma * self.gae_lambda
+                if terminal:
+                    break
+            advantages.append(A)
+        advantages = np.array(advantages, dtype=np.float)
+        return advantages
 
     def get_batch(self):
         index = np.random.randint(0, len(self.states) - self.batch_size)
@@ -33,17 +50,17 @@ class ReplayBuffer:
         states = np.array(self.states)[index:index+self.batch_size]
         actions = np.array(self.actions)[index:index+self.batch_size]
         rewards = np.array(self.rewards)[index:index+self.batch_size]
-        states_ = np.array(self.states_)[index:index+self.batch_size]
         vals = np.array(self.vals)[index:index+self.batch_size]
         probs = np.array(self.probs)[index:index+self.batch_size]
         dones = np.array(self.dones)[index:index+self.batch_size]
-        return states, actions, rewards, states_, vals, probs, dones
+        advantages = self.calc_advantages()[index:index+self.batch_size]
+
+        return states, actions, rewards, vals, probs, dones, advantages
     
     def clear_memory(self):
         self.states = []
         self.actions = []
         self.rewards = []
-        self.states_ = []
         self.vals = []
         self.probs = []
         self.dones = []
@@ -53,24 +70,24 @@ class ActorCriticNetwork(nn.Module):
     def __init__(self, lr, n_actions, input_dims, fc1_dims, fc2_dims):
         super(ActorCriticNetwork, self).__init__()
 
-        self.shared_layers = nn.Sequential(
-            nn.Linear(*input_dims, fc1_dims),
-            nn.ReLU(),
-            nn.Linear(fc1_dims, fc2_dims),
-            nn.ReLU()
-        )
-        init_linear(self.shared_layers)
-
         self.actor_network = nn.Sequential(
-            self.shared_layers,
+            nn.Linear(*input_dims, fc1_dims),
+            nn.LeakyReLU(),
+            nn.Linear(fc1_dims, fc2_dims),
+            nn.LeakyReLU(),
             nn.Linear(fc2_dims, n_actions),
             nn.Softmax(dim=-1)
         )
+        init_linear(self.actor_network)
 
         self.critic_network = nn.Sequential(
-            self.shared_layers,
+            nn.Linear(*input_dims, fc1_dims),
+            nn.LeakyReLU(),
+            nn.Linear(fc1_dims, fc2_dims),
+            nn.LeakyReLU(),
             nn.Linear(fc2_dims, 1)
         )
+        init_linear(self.critic_network)
 
         self.optimizer = optim.Adam(self.parameters(), lr=lr)
         self.device = 'cpu'#T.device('cuda:0' if T.cuda.is_available() else 'cpu')
@@ -82,8 +99,8 @@ class ActorCriticNetwork(nn.Module):
         return pi, v
 
 class Agent:
-    def __init__(self, lr, input_dims, fc1_dims, fc2_dims, n_actions, batch_size=16, \
-        horizon=40, n_updates=4, eta=0.2, gamma=0.99, gae_lambda=0.95):
+    def __init__(self, lr, input_dims, fc1_dims, fc2_dims, n_actions, batch_size=32, \
+        horizon=200, n_updates=4, eta=0.2, gamma=0.99, gae_lambda=0.95):
         self.gamma = gamma
         self.actor_critic = ActorCriticNetwork(lr, n_actions, input_dims, \
             fc1_dims, fc2_dims)
@@ -96,7 +113,7 @@ class Agent:
         self.gae_lambda = gae_lambda
         self.steps_taken = 0
         self.reward_scaler = 20
-        self.early_stop = 0.05
+        self.early_stop = 0.025
 
     def choose_action(self, observation):
         state = T.tensor(observation, dtype=T.float).to(self.actor_critic.device)
@@ -109,36 +126,23 @@ class Agent:
             self.learn()
         return action.item(), action_probs.log_prob(action).item(), value.item()
 
-    def remember(self, state, action, reward, state_, value, probs, done):
+    def remember(self, state, action, reward, value, probs, done):
         reward *= self.reward_scaler
-        self.memory.remember(state, action, reward, state_, value, probs, done)
+        self.memory.remember(state, action, reward, value, probs, done)
 
     def learn(self):
         if self.steps_taken < self.batch_size:
             return
 
         for _ in range(self.n_updates):
-            states, actions, rewards, states_, vals, probs, dones = self.memory.get_batch()
+            states, actions, rewards, vals, probs, dones, advantages = self.memory.get_batch()
 
             states = T.tensor(states, dtype=T.float).to(self.actor_critic.device)
             actions = T.tensor(actions, dtype=T.float).to(self.actor_critic.device)
             rewards = T.tensor(rewards, dtype=T.float).to(self.actor_critic.device)
-            states_ = T.tensor(states_, dtype=T.float).to(self.actor_critic.device)
             vals = T.tensor(vals, dtype=T.float).to(self.actor_critic.device)
             probs = T.tensor(probs, dtype=T.float).to(self.actor_critic.device)
             dones = T.tensor(dones, dtype=T.float).to(self.actor_critic.device)
-
-            advantages = []
-            for t in range(self.batch_size):
-                discount = 1
-                A = 0
-                for k in range(t, self.batch_size-1):
-                    delta = rewards[k] + self.gamma * vals[k+1] * (1 - int(dones[k])) - vals[k]
-                    A += discount * delta
-                    discount *= self.gamma * self.gae_lambda
-                    if dones[k]:
-                        break
-                advantages.append(A)
             advantages = T.tensor(advantages, dtype=T.float).to(self.actor_critic.device)
 
             pi, new_vals = self.actor_critic.forward(states)
@@ -152,9 +156,9 @@ class Agent:
 
             if T.abs(T.sum((new_probs - old_probs) * new_probs.exp())) > self.early_stop:
                 return
-            actor_loss = -T.min((probs_ratio * advantages), (clamped_ratio * advantages))
-            critic_loss = (advantages + (vals - new_vals))**2
-            total_loss = actor_loss.mean() + 0.5 * critic_loss.mean()
+            actor_loss = -T.min(probs_ratio * advantages, clamped_ratio * advantages).mean()
+            critic_loss = T.mean((advantages + (vals - new_vals)).pow(2))
+            total_loss = actor_loss + 0.5 * critic_loss
 
             self.actor_critic.optimizer.zero_grad()
             total_loss.backward()
