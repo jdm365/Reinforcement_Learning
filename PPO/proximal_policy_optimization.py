75c2023a51a3ca5050030e94cc1312d150d8b98c
jdm365 <jdm365@georgetown.edu>
2022-02-25 11:44:04 -0500

    ___

--- PPO/proximal_policy_optimization.py
+++ PPO/proximal_policy_optimization.py
@@ -84,7 +84,7 @@ class ActorCriticNetwork(nn.Module):
 
 class Agent:
     def __init__(self, lr, input_dims, fc1_dims, fc2_dims, n_actions, batch_size=8, \
-        horizon=32, n_updates=8, eta=0.2, gamma=0.99, gae_lambda=0.95):
+        horizon=32, n_updates=4, eta=0.2, gamma=0.99, gae_lambda=0.95):
         self.gamma = gamma
         self.actor_critic = ActorCriticNetwork(lr, n_actions, input_dims, \
             fc1_dims, fc2_dims)
