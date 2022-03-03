285d687bc2ced6cf8613b7e89b5817572ccae973
jdm365 <jdm365@georgetown.edu>
2022-03-02 20:00:26 -0500

    Adjusted ConnectN1d example.

--- AlphaZero/saved_networks.py
+++ AlphaZero/saved_networks.py
@@ -7,14 +7,13 @@ class ConnectN1dNetwork(nn.Module):
     def __init__(self, input_dims, fc1_dims, n_actions):
         super(ConnectN1dNetwork, self).__init__()
         self.shared_network = nn.Sequential(
-                    nn.Conv1d(in_channels=1, out_channels=32, kernel_size=2),
+                    nn.Conv1d(in_channels=1, out_channels=32, kernel_size=3, padding=1),
                     nn.BatchNorm1d(32),
                     nn.ReLU(),
-                    nn.Conv1d(in_channels=32, out_channels=1, kernel_size=2),
+                    nn.Conv1d(in_channels=32, out_channels=1, kernel_size=3, padding=1),
                     nn.BatchNorm1d(1),
                     nn.ReLU()
                 )
-        input_dims = input_dims - 2
 
         self.actor_head = nn.Sequential(
             self.shared_network,
