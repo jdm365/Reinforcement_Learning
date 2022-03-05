b1ed9068008e2ec96b062fe381daeaff550e123d
jdm365 <jdm365@georgetown.edu>
2022-03-05 12:13:19 -0500

    Switched original embedding convolution to take in chunks of the board. Going elementwise may have been to costly to train. Will require further testing with gpu.

--- AlphaZero/saved_networks.py
+++ AlphaZero/saved_networks.py
@@ -90,18 +90,19 @@ class PositionalEncoding(nn.Module):
     def __init__(self, input_dims, encoding_dims=768, first_block=False):
         super(PositionalEncoding, self).__init__()
         self.first_block = first_block
-        input_dims = input_dims[-2] * input_dims[-1]
-        if first_block:
-            self.cls_token = nn.Parameter(T.zeros(1, 1, encoding_dims))
-            self.pos_embed = nn.Parameter(T.zeros(1, 1 + input_dims, \
-                encoding_dims))
 
         self.encodings = nn.Conv2d(
             in_channels=1,
             out_channels=encoding_dims,
-            kernel_size=1,
-            stride=1
+            kernel_size=(2, 3),
+            stride=(2, 3),
+            padding=(0, 1)
             )
+        patch_dims = (input_dims[-2] - 2 - 1) * (input_dims[-1] - 3 - 1) + 1
+        if first_block:
+            self.cls_token = nn.Parameter(T.zeros(1, 1, encoding_dims))
+            self.pos_embed = nn.Parameter(T.zeros(1, 1 + patch_dims, \
+                encoding_dims))
 
     def forward(self, inputs):
         if not self.first_block:
@@ -163,7 +164,7 @@ class MultiHeadedAttention(nn.Module):
 
 class TransformerEncoder(nn.Module):
     def __init__(self, input_dims, encoding_dims, n_heads, \
-        fc1_dims, fc2_dims, first_block=False):
+        first_block=False):
         super(TransformerEncoder, self).__init__()
         self.multi_headed_attention = MultiHeadedAttention(
             input_dims,
@@ -175,13 +176,10 @@ class TransformerEncoder(nn.Module):
         self.norm_1 = nn.LayerNorm(encoding_dims)
 
         self.feed_forward = nn.Sequential(
-            nn.Linear(encoding_dims, fc1_dims),
-            nn.LayerNorm(fc1_dims),
+            nn.Linear(encoding_dims, encoding_dims*4),
+            nn.LayerNorm(encoding_dims*4),
             nn.GELU(),
-            nn.Linear(fc1_dims, fc2_dims),
-            nn.LayerNorm(fc2_dims),
-            nn.GELU(),
-            nn.Linear(fc2_dims, encoding_dims)
+            nn.Linear(encoding_dims*4, encoding_dims)
         )
 
         self.norm_2 = nn.LayerNorm(encoding_dims)
@@ -196,13 +194,13 @@ class TransformerEncoder(nn.Module):
 
 class TransformerNetwork(nn.Module):
     def __init__(self, input_dims, encoding_dims, n_heads, \
-        fc1_dims, fc2_dims, n_encoder_blocks):
+        n_encoder_blocks):
         super(TransformerNetwork, self).__init__()
         self.encoder_blocks = nn.ModuleList(
-            [TransformerEncoder(input_dims, encoding_dims, n_heads, fc1_dims, \
-                fc2_dims, first_block=True)] + \
-            [TransformerEncoder(input_dims, encoding_dims, n_heads, fc1_dims, \
-                fc2_dims) for _ in range(n_encoder_blocks-1)]
+            [TransformerEncoder(input_dims, encoding_dims, n_heads, \
+                first_block=True)] + \
+            [TransformerEncoder(input_dims, encoding_dims, n_heads) \
+                for _ in range(n_encoder_blocks-1)]
         )
         self.network = nn.Sequential(
             *self.encoder_blocks,
@@ -211,11 +209,11 @@ class TransformerNetwork(nn.Module):
 
 
 class Connect4NetworkTransformer(nn.Module):
-    def __init__(self, input_dims, n_actions, encoding_dims=768, n_heads=8, \
-        fc1_dims=128, fc2_dims=256, n_encoder_blocks=2):
+    def __init__(self, input_dims, n_actions, encoding_dims=768, n_heads=12, \
+        n_encoder_blocks=2):
         super(Connect4NetworkTransformer, self).__init__()
-        transformer = TransformerNetwork(input_dims, encoding_dims, n_heads, fc1_dims, \
-            fc2_dims, n_encoder_blocks)
+        transformer = TransformerNetwork(input_dims, encoding_dims, n_heads, \
+            n_encoder_blocks)
         self.network = transformer.network
 
         self.actor_head = nn.Sequential(
