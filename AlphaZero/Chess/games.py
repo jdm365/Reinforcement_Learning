8405c52c4230c5a9537d7a8e8638316c639d945c
jdm365 <jdm365@georgetown.edu>
2022-03-07 11:52:45 -0500

    Added knight moves.

--- AlphaZero/Chess/games.py
+++ AlphaZero/Chess/games.py
@@ -640,6 +640,16 @@ class ChessRules:
                     north_west_range = 1
                     break
 
+
+                possible_moves_array[arr_coords[0], arr_coords[1], 0:north_range] = 1
+                possible_moves_array[arr_coords[0], arr_coords[1], 14:14+east_range] = 1
+                possible_moves_array[arr_coords[0], arr_coords[1], 28:28+south_range] = 1
+                possible_moves_array[arr_coords[0], arr_coords[1], 42:42+west_range] = 1
+                possible_moves_array[arr_coords[0], arr_coords[1], 7:7+north_east_range] = 1
+                possible_moves_array[arr_coords[0], arr_coords[1], 21:21+south_east_range] = 1
+                possible_moves_array[arr_coords[0], arr_coords[1], 35:35+south_west_range] = 1
+                possible_moves_array[arr_coords[0], arr_coords[1], 49:49+north_west_range] = 1
+
             
             if square == 'K':
                 if (arr_coords[0] + 1) > 7:
@@ -743,9 +753,251 @@ class ChessRules:
                     break
 
 
-            if square == 'n'
+                possible_moves_array[arr_coords[0], arr_coords[1], 0:north_range] = 1
+                possible_moves_array[arr_coords[0], arr_coords[1], 14:14+east_range] = 1
+                possible_moves_array[arr_coords[0], arr_coords[1], 28:28+south_range] = 1
+                possible_moves_array[arr_coords[0], arr_coords[1], 42:42+west_range] = 1
+                possible_moves_array[arr_coords[0], arr_coords[1], 7:7+north_east_range] = 1
+                possible_moves_array[arr_coords[0], arr_coords[1], 21:21+south_east_range] = 1
+                possible_moves_array[arr_coords[0], arr_coords[1], 35:35+south_west_range] = 1
+                possible_moves_array[arr_coords[0], arr_coords[1], 49:49+north_west_range] = 1
+
+
+
+            if square == 'n':
+                if (arr_coords[0] + 2) > 7 or (arr_coords[1] + 1) > 7:
+                    north_east_move = 0
+                    break
+                north_east_square = board_array[arr_coords[0]+2, arr_coords[1]+1]
+                north_east_move = 1
+                if north_east_square.islower():
+                    north_east_move = 0
+                    break
+                if north_east_square.isupper():
+                    north_east_move = 1
+                    break
+
+
+                if (arr_coords[0] + 1) > 7 or (arr_coords[1] + 2) > 7:
+                    east_north_move = 0
+                    break
+                east_north_square = board_array[arr_coords[0]+1, arr_coords[1]+2]
+                east_north_move = 1
+                if east_north_square.islower():
+                    east_north_move = 0
+                    break
+                if north_east_square.isupper():
+                    east_north_move = 1
+                    break
+                
+
+                if (arr_coords[0] - 1) < 0 or (arr_coords[1] + 2) > 7:
+                    east_south_move = 0
+                    break
+                east_south_square = board_array[arr_coords[0]-1, arr_coords[1]+2]
+                east_south_move = 1
+                if east_south_square.islower():
+                    east_south_move = 0
+                    break
+                if east_south_square.isupper():
+                    east_south_move = 1
+                    break
+
+
+                if (arr_coords[0] - 2) < 0 or (arr_coords[1] + 1) > 7:
+                    south_east_move = 0
+                    break
+                south_east_square = board_array[arr_coords[0]-2, arr_coords[1]+1]
+                south_east_move = 1
+                if south_east_square.islower():
+                    south_east_move = 0
+                    break
+                if south_east_square.isupper():
+                    south_east_move = 1
+                    break
+
+
+                if (arr_coords[0] - 2) < 0 or (arr_coords[1] - 1) < 0:
+                    south_west_move = 0
+                    break
+                south_west_square = board_array[arr_coords[0]-2, arr_coords[1]-1]
+                south_west_move = 1
+                if south_west_square.islower():
+                    south_west_move = 0
+                    break
+                if south_west_square.isupper():
+                    south_west_move = 1
+                    break
+
+
+                if (arr_coords[0] - 1) < 0 or (arr_coords[1] - 2) < 0:
+                    west_south_move = 0
+                    break
+                west_south_square = board_array[arr_coords[0]-2, arr_coords[1]-1]
+                west_south_move = 1
+                if west_south_square.islower():
+                    west_south_move = 0
+                    break
+                if west_south_square.isupper():
+                    west_south_move = 1
+                    break
+
+
+                if (arr_coords[0] + 1) > 7 or (arr_coords[1] - 2) < 0:
+                    west_north_move = 0
+                    break
+                west_north_square = board_array[arr_coords[0]+1, arr_coords[1]-2]
+                west_north_move = 1
+                if west_north_square.islower():
+                    west_north_move = 0
+                    break
+                if west_north_square.isupper():
+                    west_north_move = 1
+                    break
+
+
+                if (arr_coords[0] + 2) > 7 or (arr_coords[1] - 1) < 0:
+                    north_west_move = 0
+                    break
+                north_west_move = board_array[arr_coords[0]+2, arr_coords[1]-1]
+                north_west_move = 1
+                if north_west_move.islower():
+                    north_west_move = 0
+                    break
+                if north_west_move.isupper():
+                    north_west_move = 1
+                    break
+
+
+                possible_moves_array[arr_coords[0], arr_coords[1], 56] = north_east_move
+                possible_moves_array[arr_coords[0], arr_coords[1], 57] = east_north_move
+                possible_moves_array[arr_coords[0], arr_coords[1], 58] = east_south_move
+                possible_moves_array[arr_coords[0], arr_coords[1], 59] = south_east_move
+                possible_moves_array[arr_coords[0], arr_coords[1], 60] = south_west_move
+                possible_moves_array[arr_coords[0], arr_coords[1], 61] = west_south_move
+                possible_moves_array[arr_coords[0], arr_coords[1], 62] = west_north_move
+                possible_moves_array[arr_coords[0], arr_coords[1], 63] = north_west_move
+
+
+
+            if square == 'N':
+                if (arr_coords[0] + 2) > 7 or (arr_coords[1] + 1) > 7:
+                    north_east_move = 0
+                    break
+                north_east_square = board_array[arr_coords[0]+2, arr_coords[1]+1]
+                north_east_move = 1
+                if north_east_square.isupper():
+                    north_east_move = 0
+                    break
+                if north_east_square.islower():
+                    north_east_move = 1
+                    break
+
+
+                if (arr_coords[0] + 1) > 7 or (arr_coords[1] + 2) > 7:
+                    east_north_move = 0
+                    break
+                east_north_square = board_array[arr_coords[0]+1, arr_coords[1]+2]
+                east_north_move = 1
+                if east_north_square.isupper():
+                    east_north_move = 0
+                    break
+                if north_east_square.islower():
+                    east_north_move = 1
+                    break
+                
+
+                if (arr_coords[0] - 1) < 0 or (arr_coords[1] + 2) > 7:
+                    east_south_move = 0
+                    break
+                east_south_square = board_array[arr_coords[0]-1, arr_coords[1]+2]
+                east_south_move = 1
+                if east_south_square.isupper():
+                    east_south_move = 0
+                    break
+                if east_south_square.islower():
+                    east_south_move = 1
+                    break
+
+
+                if (arr_coords[0] - 2) < 0 or (arr_coords[1] + 1) > 7:
+                    south_east_move = 0
+                    break
+                south_east_square = board_array[arr_coords[0]-2, arr_coords[1]+1]
+                south_east_move = 1
+                if south_east_square.isupper():
+                    south_east_move = 0
+                    break
+                if south_east_square.islower():
+                    south_east_move = 1
+                    break
+
+
+                if (arr_coords[0] - 2) < 0 or (arr_coords[1] - 1) < 0:
+                    south_west_move = 0
+                    break
+                south_west_square = board_array[arr_coords[0]-2, arr_coords[1]-1]
+                south_west_move = 1
+                if south_west_square.isupper():
+                    south_west_move = 0
+                    break
+                if south_west_square.islower():
+                    south_west_move = 1
+                    break
+
+
+                if (arr_coords[0] - 1) < 0 or (arr_coords[1] - 2) < 0:
+                    west_south_move = 0
+                    break
+                west_south_square = board_array[arr_coords[0]-2, arr_coords[1]-1]
+                west_south_move = 1
+                if west_south_square.isupper():
+                    west_south_move = 0
+                    break
+                if west_south_square.islower():
+                    west_south_move = 1
+                    break
+
+
+                if (arr_coords[0] + 1) > 7 or (arr_coords[1] - 2) < 0:
+                    west_north_move = 0
+                    break
+                west_north_square = board_array[arr_coords[0]+1, arr_coords[1]-2]
+                west_north_move = 1
+                if west_north_square.isupper():
+                    west_north_move = 0
+                    break
+                if west_north_square.islower():
+                    west_north_move = 1
+                    break
+
+
+                if (arr_coords[0] + 2) > 7 or (arr_coords[1] - 1) < 0:
+                    north_west_move = 0
+                    break
+                north_west_move = board_array[arr_coords[0]+2, arr_coords[1]-1]
+                north_west_move = 1
+                if north_west_move.isupper():
+                    north_west_move = 0
+                    break
+                if north_west_move.islower():
+                    north_west_move = 1
+                    break
+
+
+                possible_moves_array[arr_coords[0], arr_coords[1], 56] = north_east_move
+                possible_moves_array[arr_coords[0], arr_coords[1], 57] = east_north_move
+                possible_moves_array[arr_coords[0], arr_coords[1], 58] = east_south_move
+                possible_moves_array[arr_coords[0], arr_coords[1], 59] = south_east_move
+                possible_moves_array[arr_coords[0], arr_coords[1], 60] = south_west_move
+                possible_moves_array[arr_coords[0], arr_coords[1], 61] = west_south_move
+                possible_moves_array[arr_coords[0], arr_coords[1], 62] = west_north_move
+                possible_moves_array[arr_coords[0], arr_coords[1], 63] = north_west_move
+
 
             
+
+
 
 
 
